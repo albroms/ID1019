3cb50480371160b12d963cc0281ca8d900d83c31
albroms <albroms@kth.se>
2020-01-22 15:09:27 +0100

    began work on variables

--- Exercises/calculate.ex
+++ Exercises/calculate.ex
@@ -1,7 +1,11 @@
 defmodule Calculate do
+  #var = {:var, name}
+
   def eval({:int, n}) do n end
   def eval({:add, a, b}) do eval(a) + eval(b) end
   def eval({:sub, a, b}) do eval(a) - eval(b) end
   def eval({:mul, a, b}) do eval(a) * eval(b) end
   ## TODO: implement variables
+  def lookup(var, [{:bind, var, value} | _]) do value end
+  def lookup(var, [_ | rest]) do lookup(var, rest) end
 end
