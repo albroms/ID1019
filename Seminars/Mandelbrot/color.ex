ed71f3734132f5f804c72b1212fbdfdea4de6806
albroms <albroms@kth.se>
2020-02-11 18:54:03 +0100

    Mandelbrot done.

---
+++ Seminars/Mandelbrot/color.ex
@@ -0,0 +1,121 @@
+defmodule Color do
+  # export function
+  # given a depth on a scale from 0 to max, give us a color.
+  def convert(depth, max) do
+    #red(depth, max)
+    #blue(depth, max)
+    #green_cool(depth, max)
+    green_warm(depth, max)
+    #inverted(depth, max)
+  end
+
+  def red(d, max) do
+    #from red to blue
+    f = d / max
+
+    # a is from 0 to 4
+    a = f * 4
+
+    # x should be 0..4
+    x = trunc(a)
+
+    # y should be 0..255
+    y = trunc(255 * (a-x) )
+
+    case x do
+      0 ->
+        {:rgb, y, 0, 0} #black -> red
+      1 ->
+        {:rgb, 255, y, 0} #red -> yellow
+      2 ->
+        {:rgb, 255-y, 255, 0} #yellow -> green
+      3 ->
+        {:rgb, 0, 255, y} #green -> cyan
+      4 ->
+        {:rgb, 0, 255-y, 255} #cyan -> blue
+    end
+  end
+
+  def blue(d, max) do
+    #from blue to red
+
+    f = d / max
+    a = f * 4
+    x = trunc(a)
+    y = trunc(255 * (a - x) )
+
+    case x do
+      0 ->
+        {:rgb, 0, 0, y} #black -> blue
+      1 ->
+        {:rgb, 0, y, 255} #blue -> cyan
+      2 ->
+        {:rgb, 0, 255, 255-y} #cyan -> green
+      3 ->
+        {:rgb, y, 255, 0} #green -> yellow
+      4 ->
+        {:rgb, 255, 255-y, 0} #yellow -> red
+    end
+  end
+
+  def green_warm(d, max) do
+    f = d / max
+    a = f * 4
+    x = trunc(a)
+    y = trunc(255 * (a-x) )
+
+    case x do
+      0 ->
+        {:rgb, 0, y, 0} #black -> green
+      1 ->
+        {:rgb, y, 255, 0} #green -> yellow
+      2 ->
+        {:rgb, 255, 255-y, 0} #yellow -> red
+      3 ->
+        {:rgb, 255-y, 0, y} #red -> purple
+      4 ->
+        {:rgb, 0, y, 255-y} #purple -> blue
+    end
+  end
+
+  def green_cool(d, max) do
+    f = d / max
+    a = f * 4
+    x = trunc(a)
+    y = trunc(255 * (a-x) )
+
+    case x do
+      0 ->
+        {:rgb, 0, y, 0} #black -> green
+      1 ->
+        {:rgb, 0, 255, y} #green -> cyan
+      2 ->
+        {:rgb, 0, 255-y, 255} #cyan -> blue
+      3 ->
+        {:rgb, y, 0, 255} #blue -> purple
+      4 ->
+        {:rgb, 255, 0, 255-y} #purple -> red
+    end
+  end
+
+  def inverted(d, max) do
+    f = d / max
+    a = f * 4
+    x = trunc(a)
+    y = trunc(255 * (a-x) )
+
+    case x do
+      0 ->
+        {:rgb, 255, 255, 255-y} # white -> yellow
+      1 ->
+        {:rgb, 255, 255-y, 0} #yellow -> red
+      2 ->
+        {:rgb, 255, 0, y} #red -> purple
+      3 ->
+        {:rgb, 255-y, 0, 255} #purple -> blue
+      4 ->
+        {:rgb, 0, y, 255-y} #blue -> cyan
+    end
+  end
+
+end
---
+++ Seminars/Mandelbrot/test.ex
@@ -0,0 +1,14 @@
+defmodule Test do
+  def demo() do
+    small(-0.85, 0.1, 0.89)
+  end
+
+  def small(x0, y0, xn) do
+    width = 960
+    height = 540
+    depth = 64
+    k = (xn - x0) / width
+    image = Mandel.mandelbrot(width, height, x0, y0, k, depth)
+    PPM.write("small.ppm", image)
+  end
+end
