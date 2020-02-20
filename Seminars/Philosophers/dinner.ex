19244703321dca823d8e7d2e1fe25f61d3b79e68
albroms <albroms@kth.se>
2020-02-20 23:43:05 +0100

    gui added, still needs work

--- Seminars/Philosophers/dinner.ex
+++ Seminars/Philosophers/dinner.ex
@@ -1,30 +1,37 @@
 defmodule Dinner do
-  def start(), do: spawn(fn -> init() end)
+  def start() do
+    dinner = spawn(fn -> init() end)
+    Process.register(dinner, :dinner)
+  end
 
   def init() do
+    seed = 1234
     c1 = Chopstick.start()
     c2 = Chopstick.start()
     c3 = Chopstick.start()
     c4 = Chopstick.start()
     c5 = Chopstick.start()
     ctrl = self() #control process
-    Philosopher.start(5, c1, c2, "Jinping", ctrl, seed + 1)
-    Philosopher.start(5, c2, c3, "Donald", ctrl, seed + 2)
-    Philosopher.start(5, c3, c4, "Vladimir", ctrl, seed + 3)
-    Philosopher.start(5, c4, c5, "Boris", ctrl, seed + 4)
-    Philosopher.start(5, c5, c1, "Shinzo", ctrl, seed + 5)
+    Philosopher.start(5, 5, c1, c2, "Jinping", ctrl, seed + 1)
+    Philosopher.start(5, 5, c2, c3, "Donald", ctrl, seed + 2)
+    Philosopher.start(5, 5, c3, c4, "Vladimir", ctrl, seed + 3)
+    Philosopher.start(5, 5, c4, c5, "Boris", ctrl, seed + 4)
+    Philosopher.start(5, 5, c5, c1, "Shinzo", ctrl, seed + 5)
     wait(5, [c1, c2, c3, c4, c5])
   end
 
-  def wait(0, chopsticks) do
-    Enum.each(chopstick, fn(c) -> Chopstick.quit(c) end)
+  defp wait(0, chopsticks) do
+    Enum.each(chopsticks, fn(c) -> Chopstick.quit(c) end)
+    Process.unregister(:dinner)
   end
 
-  def wait(n, chopsticks) do
+  defp wait(n, chopsticks) do
     receive do
       :done ->
         wait(n-1, chopsticks)
       :abort ->
+        # our kill switch
+        :io.format("dinner aborted~")
         Process.exit(self(), :kill)
     end
   end
--- Seminars/Philosophers/philosopher.ex
+++ Seminars/Philosophers/philosopher.ex
@@ -1,35 +1,78 @@
 defmodule Philosopher do
 
-  @dream 1000
-  @eat     50
-  @delay  200
+  @dream 10000
+  @eat     500
+  @delay  2000
 
-  def sleep(0) do :ok end
-  def sleep(t) do
-    :timer.sleep(:rand.uniform(t))
-  end
+  defp delay(t), do: sleep(t)
+
+  defp sleep(0), do: :ok
+  defp sleep(t), do: :timer.sleep(:rand.uniform(t))
 
   def start(hunger, strength, right, left, name, ctrl, seed) do
-    #spawn a philosopher process
+    IO.puts("#{name} sat at the table")
+    delay(@delay)
     spawn_link(fn -> init(hunger, strength, right, left, name, ctrl, seed) end)
   end
 
   defp init(hunger, strength, right, left, name, ctrl, seed) do
-    gui = Gui.start(name)
+    gui = Gui.start(name) #Each philosopher has a gui associated with it.
     :rand.seed(:exsplus, {seed, seed, seed})
     dreaming(hunger, strength, right, left, name, ctrl, gui)
   end
 
-  def dreaming do
-    IO.puts("#{name} is dreaming.")
+  #Dreaming state:
+  defp dreaming(0, strength, _right, _left, name, ctrl, gui) do
+    IO.puts("#{name} is happy, strength is still #{strength}!")
+    send(gui, :stop)
+    send(ctrl, :done)
   end
 
-  def waiting do
-    IO.puts("#{name} is waiting. #{hunger} hunger to go")
+  defp dreaming(hunger, 0, _right, _left, name, ctrl, gui) do
+    IO.puts("#{name} starved to death, hunger has reached #{hunger}")
+    send(gui, :stop)
+    send(ctrl, :done)
   end
 
-  def eating do
-    IO.puts("#{name} is eating.")
+  defp dreaming(hunger, strength, right, left, name, ctrl, gui) do
+    IO.puts("#{name} is sleeping...")
+    delay(@dream)
+    IO.puts("#{name} woke up! Strength is at #{strength}.")
+    waiting(hunger, strength, right, left, name, ctrl, gui)
+  end
+
+
+  #Waiting state:
+  defp waiting(hunger, strength, right, left, name, ctrl, gui) do
+    send(gui, :waiting)
+    IO.puts("#{name} is waiting to eat. #{hunger} hunger to go")
+    case Chopstick.request(right) do
+      :ok ->
+        IO.puts("#{name} took the right chopstick!")
+        delay(@delay)
+        case Chopstick.request(left) do
+          :ok ->
+            IO.puts("#{name} now has both chopsticks and can begin eating!")
+            eating(hunger, strength, right, left, name, ctrl, gui)
+            send(gui, :leave)
+            dreaming(hunger, strength, right, left, name, ctrl, gui)
+        end
+    end
+  end
+
+  #Eating state:
+  defp eating(hunger, strength, right, left, name, ctrl, gui) do
+    send(gui, :enter)
+    IO.puts("#{name} is eating...")
+
+    delay(@eat)
+
+    Chopstick.return(left)
+    Chopstick.return(right)
+
+    IO.puts("#{name} returned the chopsticks and fell asleep...")
+    send(gui, :leave)
+    dreaming(hunger-1, strength, right, left, name, ctrl, gui)
   end
 
 end
